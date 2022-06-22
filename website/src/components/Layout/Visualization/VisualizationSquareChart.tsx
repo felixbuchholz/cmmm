af4fd5a82e3431d6f5b7e1553203ba85b34754f1
Felix Buchholz <felix@felixbuchholz.com>
2022-06-22 18:52:37 +0200

    feat(squar-chart): add square-chart with fade static sized squares

--- website/src/components/Layout/Visualization/VisualizationSquareChart.tsx
+++ website/src/components/Layout/Visualization/VisualizationSquareChart.tsx
@@ -11,12 +11,17 @@ export const VisualizationSquareChart: FC<{
   const count = useCount(featurePropertiesKey)
   const arr = Array.from(Array(count).keys())
   return (
-    <div>
-      <h3>How many offers are presented in Belgrade?</h3>
-      <div>
-        {arr.map(it => (
-          <div key={it}>.</div>
-        ))}
+    <div className={styles.container}>
+      <div className={styles.headline}>
+        How many offers are presented in Belgrade?
+      </div>
+      <div className={styles.chart}>
+        <div className={styles.squareContainer}>
+          {arr.map(it => (
+            <div key={it} className={styles.square}></div>
+          ))}
+        </div>
+        <div className={styles.fade}></div>
       </div>
     </div>
   )

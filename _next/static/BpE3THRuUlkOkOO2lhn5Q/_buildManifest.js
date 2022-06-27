eac1dc4dc34564b1634d0adf3c63d73f44037041
felixbuchholz <felixbuchholz@users.noreply.github.com>
2022-06-27 17:20:50 +0000

    deploy: bd5217d8a4dfcd613212c8d3a6d6f283ff30dd3d

--- _next/static/BpE3THRuUlkOkOO2lhn5Q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/557-1bafd86225a4b723.js","static/css/1b49d88ab6b63dc9.css","static/chunks/pages/index-d60ad24e9a95b22e.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

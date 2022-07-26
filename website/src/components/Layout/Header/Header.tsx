93b86798a6f4771a4163a14a34ae89a545684909
Felix Buchholz <32803817+felixbuchholz@users.noreply.github.com>
2022-07-26 16:35:56 +0200

    feat(header): add site title and project logo link

--- website/src/components/Layout/Header/Header.tsx
+++ website/src/components/Layout/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'next-export-i18n'
 import React, { FC } from 'react'
 
+import { CMMMButton } from './CMMMButton'
 import styles from './header.module.css'
 import { InfoPageLink } from './InfoPageLink'
 
@@ -9,8 +10,9 @@ export const Header: FC = () => {
   return (
     <header className={styles.container}>
       <h1 className="subheadline">
-        <InfoPageLink /> {t('site.title')}
+        <InfoPageLink /> <span className={styles.title}>{t('site.title')}</span>
       </h1>
+      <CMMMButton />
     </header>
   )
 }

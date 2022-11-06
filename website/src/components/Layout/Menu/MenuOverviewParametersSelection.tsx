b389acf2e2dbe3b3d4fdaa77533a5aa699368a55
Felix Buchholz <32803817+felixbuchholz@users.noreply.github.com>
2022-11-06 18:38:50 +0100

    feat(menuOverview): switch order of scenario and param selection

--- website/src/components/Layout/Menu/MenuOverviewParametersSelection.tsx
+++ website/src/components/Layout/Menu/MenuOverviewParametersSelection.tsx
@@ -4,27 +4,36 @@ import { ParameterSelection } from '../../../types/MenusParameter'
 import { OptionsSelectionButtons } from '../Navigation/OptionsSelectionButtons'
 import { ScenarioSelectionButton } from '../Navigation/ScenarioSelectionButton'
 
+import styles from './menuOverview.module.css'
 import { MenuOverviewParametersSelectionItem } from './MenuOverviewParametersSelectionItem'
 
 export const MenuOverviewParametersSelection: FC = () => {
   return (
     <>
-      {parameterSelections.map(it => (
-        <MenuOverviewParametersSelectionItem key={it.translateKey} {...it} />
+      {parameterSelections.map((it, index) => (
+        <>
+          <MenuOverviewParametersSelectionItem key={it.translateKey} {...it} />
+          {index === dividerIndex && (
+            <div className={styles.snug}>
+              <h3>or</h3>
+            </div>
+          )}
+        </>
       ))}
     </>
   )
 }
+const dividerIndex = 0
 
 const parameterSelections: ParameterSelection[] = [
-  {
-    translateKey: 'menuOverview.scenarioSelectExplainer',
-    activeStates: ['menu.mode_scenario'],
-    children: <ScenarioSelectionButton />,
-  },
   {
     translateKey: 'menuOverview.optionsSelectExplainer',
     activeStates: ['menu.mode_options'],
     children: <OptionsSelectionButtons />,
+  },
+  {
+    translateKey: 'menuOverview.scenarioSelectExplainer',
+    activeStates: ['menu.mode_scenario'],
+    children: <ScenarioSelectionButton />,
   },
 ]

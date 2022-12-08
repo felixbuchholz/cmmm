b8dbdae19bbb23e226e892707f9c3aa4cb141a5f
Felix Buchholz <32803817+felixbuchholz@users.noreply.github.com>
2022-12-08 11:24:15 +0100

    feat(menu): update scenario selection button state

--- website/src/components/Layout/Navigation/ScenarioSelectionButton.tsx
+++ website/src/components/Layout/Navigation/ScenarioSelectionButton.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'next-export-i18n'
 import React, { FC } from 'react'
 
 import {
+  useResolveStateArrayToBool,
   useSelectionContext,
   useSelectionSend,
 } from '../../../state/selection/hooks'
@@ -22,11 +23,10 @@ export const ScenarioSelectionButtonIntro: FC = () => {
 }
 
 export const ScenarioSelectionButtons: FC = () => {
+  const isOptionsMode = useResolveStateArrayToBool(['menu.mode_options'])
   const [income, size] = useSelectionContext(['income', 'size'])
-  const matchedScenario = scenarioParameters.find(
-    it => it.income == income && it.size === size
-  )
-  if (!income || !size) {
+
+  if (!income || !size || isOptionsMode) {
     return (
       <>
         <ScenarioSelectionButtonIntro />
@@ -34,16 +34,12 @@ export const ScenarioSelectionButtons: FC = () => {
       </>
     )
   }
-  if (!matchedScenario) {
-    return (
-      <>
-        <ScenarioSelectionButtonIntro />
-        <ScenarioSelectButtonSelected />
-      </>
-    )
-  }
 
+  const matchedScenario = scenarioParameters.find(
+    it => it.income == income && it.size === size
+  )
   const translateKey = matchedScenario.translateKey
+
   return <ScenarioSelectButtonScenario translateKey={translateKey} />
 }
 
@@ -97,7 +93,7 @@ export const ScenarioSelectButtonScenario: FC<{ translateKey: string }> = ({
       <button className="triangle" onClick={handleClick}>
         <span className="buttonSpan">{t(translateKey)}</span>
         <br />
-        <span className="comparison">
+        <span className="text-black">
           ({size} {t(`person${p(size)}`)}, {income} {t('units.euroPerMonth')})
         </span>
       </button>
